23d9082ea04f3fa419742600a0865b07743e558f
Ramin <ramin_n11@icloud.com>
2023-09-19 22:11:18 +0300

    Выполнил домашнее задание

---
+++ Number4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+int even = 2;
+while (n >= even)
+{
+    Console.WriteLine(even + " ");
+    even = even + 2;
+}
